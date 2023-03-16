383b5c3707c40ea8f2ed74c5fc8ff66d32d3c2ea
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-03-16 20:14:01 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+startBtn.addEventListener('click', changeBgColor);
+
+let timerId;
+
+function changeBgColor() {
+    startBtn.setAttribute('disabled', 'true');
+   
+   timerId = setInterval(() => {
+        let bgColor = getRandomHexColor();
+        bodyEl.style.backgroundColor = bgColor;;
+      }, 1000);
+}
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled', 'true');
+  });
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
